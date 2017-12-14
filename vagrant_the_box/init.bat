d83b3dd33176a819c7c02adb65013d88e4f61e78
Sergi Rusiñol Vellvehi <sergirusi@gmail.com>
2017-12-14 12:53:48 +0100

    Improved init script i dashboard

--- vagrant_the_box/init.bat
+++ vagrant_the_box/init.bat
@@ -3,4 +3,5 @@ START cmd /k vagrant ssh -c 'cd /vagrant/elk; docker-compose up; sudo su'
 TIMEOUT 10
 START cmd /k vagrant ssh -c 'cd /vagrant/scripts; /vagrant/scripts/restart.sh; sudo su'
 START cmd /k vagrant ssh -c 'cd /vagrant/elk; ./dockbeat-v1.0.0-x86_64 -c dockbeat/dockbeat.yml -v -e; sudo su'
+TIMEOUT 10
 START cmd /k vagrant ssh -c 'cd /vagrant/elk; /vagrant/scripts/emul_petitions.sh; sudo su'
